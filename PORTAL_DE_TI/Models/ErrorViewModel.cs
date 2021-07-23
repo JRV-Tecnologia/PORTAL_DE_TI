938ad3b1b54d4761a1f86cd6d1a87f4ae75696f0
nkllsrvs <nikollas07@gmail.com>
2021-07-23 12:44:19 +0000

    Add project files.

---
+++ PORTAL_DE_TI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PORTAL_DE_TI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
