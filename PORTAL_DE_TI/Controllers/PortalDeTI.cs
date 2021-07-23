44d88053e1ab63dfa3baab6b7923796c30596265
nkllsrvs <nikollas07@gmail.com>
2021-07-23 18:09:22 +0000

    teste

---
+++ PORTAL_DE_TI/Controllers/PortalDeTI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PORTAL_DE_TI.Controllers
+{
+    public class PortalDeTI : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
