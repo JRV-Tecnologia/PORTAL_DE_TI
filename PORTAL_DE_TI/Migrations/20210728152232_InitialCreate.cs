5339278d1481b6376df76d096dad2fa13b5fc4d5
Doglast <des212425@gmail.com>
2021-07-28 17:07:18 +0000

    .

---
+++ PORTAL_DE_TI/Migrations/20210728152232_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PORTAL_DE_TI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TB_News",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    NOME = table.Column<string>(nullable: true),
+                    RESENHA = table.Column<string>(nullable: true),
+                    CAMINHO_IMG = table.Column<string>(nullable: true),
+                    TEXTO = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TB_News", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TB_News");
+        }
+    }
+}
---
+++ PORTAL_DE_TI/Models/Model.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PORTAL_DE_TI.Models
+{
+
+    public class PortalContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public PortalContext()
+        {
+            _connectionString = @"Data Source=Desenvolvimento\SQLEXPRESS;User ID=admin;Password=admin@jrv;Database=DB_Portal_TI;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        }
+
+        public PortalContext(DbContextOptions<PortalContext> options)
+            : base(options)
+        { }
+
+        public DbSet<NewsDB> NewsDBs { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
