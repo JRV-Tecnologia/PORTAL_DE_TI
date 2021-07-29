5aa7d0f6bfd9a020e1a365ded801cac25a72dac2
Doglast <des212425@gmail.com>
2021-07-29 21:46:44 +0000

    .

---
+++ PORTAL_DE_TI/Controllers/DetalhesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PORTAL_DE_TI.Controllers
+{
+    public class DetalhesController : PrivateController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
